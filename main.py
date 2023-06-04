581633c1896b0040c4a95606aab0db70c827bfb8
Cade Gordon <cadergator1@hotmail.com>
2023-06-04 10:06:47 -0400

    initial files

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Anki cards are a great way to learn and memorize anything. 
+This program continually prompts for textbook passages and
+can turn them into valid anki cards automatically.
+"""
+
+
+def main():
+    # Code here
+    pass
+
+
+if __name__ == "__main__":
+    main()
